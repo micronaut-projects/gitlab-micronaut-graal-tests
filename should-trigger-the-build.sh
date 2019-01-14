0c2bb0c092b0ebe5907b7f2b30018b53fb051d58
Iván López <lopez.ivan@gmail.com>
2019-01-14 15:37:12 +0100

    Update files with Micronaut and Graal commit hashes after a new one is found

--- should-trigger-the-build.sh
+++ should-trigger-the-build.sh
@@ -15,6 +15,25 @@ echo "Micronaut current commit: $MN_CURRENT_COMMIT"
 echo "Micronaut last commit: $MN_LAST_COMMIT"
 
 if [ "$GRAAL_CURRENT_COMMIT" != "$GRAAL_LAST_COMMIT" ] || [ "$MN_CURRENT_COMMIT" != "$MN_LAST_COMMIT" ]; then
-	echo "Something changed, triggering the build..."
-	touch .trigger_build
+    echo "Something changed, triggering the build..."
+    touch .trigger_build
+
+    # Commit the new hashes to the same branch
+    export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i .ssh/id_rsa-gitlab-ci"
+
+    mkdir -pvm 0700 .ssh
+    echo "$SSH_PRIVATE_KEY" > .ssh/id_rsa-gitlab-ci
+    chmod 0400 .ssh/id_rsa-gitlab-ci
+    git checkout $CI_BUILD_REF_NAME
+    git config user.email "$(echo $GITLAB_USER_EMAIL)"
+    git config user.name "$(echo $GITLAB_USER_NAME)"
+
+    cp .tmp_graal .graal_master_commit
+    cp .tmp_micronaut .mn_master_commit
+
+    git add .graal_master_commit .mn_master_commit
+    git commit -m "Update Micronaut and GraalVM last commits [ci skip]"
+
+    git remote set-url --push origin $(perl -pe 's#.*@(.+?(\:\d+)?)/#git@\1:#' <<< $CI_REPOSITORY_URL)
+    git push --all
 fi;
