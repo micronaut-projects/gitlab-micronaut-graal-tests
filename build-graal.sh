90e4706bd4edf8820fa78bd37681820a15c7d4bb
Iván López <lopez.ivan@gmail.com>
2019-01-14 11:40:19 +0100

    Reorder build-graal script

--- build-graal.sh
+++ build-graal.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 echo "Building Graal..."
+
 mkdir graal
 cd graal
-git clone https://github.com/oracle/graal
-git clone https://github.com/graalvm/mx
+
 wget -q https://github.com/graalvm/openjdk8-jvmci-builder/releases/download/jvmci-0.53/openjdk-8u192-jvmci-0.53-linux-amd64.tar.gz
 tar zxf openjdk-8u192-jvmci-0.53-linux-amd64.tar.gz
 export PATH=$PWD/mx:$PATH
 export JAVA_HOME=$CI_PROJECT_DIR/graal/openjdk1.8.0_192-jvmci-0.53
+
+git clone https://github.com/oracle/graal
+git clone https://github.com/graalvm/mx
+
 cd graal/vm
 mx clean
 mx --disable-polyglot --disable-libpolyglot --dynamicimports /substratevm build
