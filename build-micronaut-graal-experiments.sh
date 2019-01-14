c1a3a14ce52e05821bf4b0d576e4296a7d039a85
Iván López <lopez.ivan@gmail.com>
2019-01-14 12:41:44 +0100

    Use jvmci-0.53 java version for building fatjar

--- build-micronaut-graal-experiments.sh
+++ build-micronaut-graal-experiments.sh
@@ -6,6 +6,10 @@ export GRAALVM_HOME=$CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home
 export PATH=$GRAALVM_HOME/bin:$PATH
 native-image --version
 
+export JAVA_HOME=$CI_PROJECT_DIR/graal/openjdk1.8.0_192-jvmci-0.53
+export PATH=$JAVA_HOME/bin:$PATH
+java -version
+
 git clone https://github.com/graemerocher/micronaut-graal-experiments.git
 cd micronaut-graal-experiments/fresh-graal
 ./gradlew --no-daemon --console=plain assemble
