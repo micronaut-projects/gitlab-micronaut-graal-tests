c3e2bd0100fee382be766e73c59787012071e664
Iván López <lopez.ivan@gmail.com>
2019-01-15 10:05:37 +0100

    Adjust log messages

--- log-commits.sh
+++ log-commits.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running build..."
-echo " - Graal current commit: $GRAAL_CURRENT_COMMIT"
-echo " - Graal last commit: $GRAAL_LAST_COMMIT"
-echo " - Micronaut current commit: $MN_CURRENT_COMMIT"
-echo " - Micronaut last commit: $MN_LAST_COMMIT"
+echo "Graal current commit: $GRAAL_CURRENT_COMMIT"
+echo "Graal last commit: $GRAAL_LAST_COMMIT"
+echo "Micronaut current commit: $MN_CURRENT_COMMIT"
+echo "Micronaut last commit: $MN_LAST_COMMIT"
