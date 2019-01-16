78a94c09a4a1345ea7ac0332ca9c76ffb5f3a92b
Iván López <lopez.ivan@gmail.com>
2019-01-16 10:00:19 +0100

    Use GraalVM for graal-experiments

--- build-micronaut-graal-experiments.sh
+++ build-micronaut-graal-experiments.sh
@@ -2,13 +2,10 @@
 
 echo "Building micronaut-graal-experiments..."
 
-export GRAALVM_HOME=$CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home
-export PATH=$GRAALVM_HOME/bin:$PATH
-native-image --version
-
-export JAVA_HOME=$CI_PROJECT_DIR/graal/openjdk1.8.0_192-jvmci-0.53
+export JAVA_HOME=$CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home
 export PATH=$JAVA_HOME/bin:$PATH
 java -version
+native-image --version
 
 git clone https://github.com/micronaut-graal-tests/micronaut-graal-experiments.git
 cd micronaut-graal-experiments/fresh-graal
