7896c072e49587cba80fa489c06ffcdea51a9639
Iván López <lopez.ivan@gmail.com>
2019-01-17 10:14:25 +0100

    Clone the right application

--- build-micronaut-consul-http-client.sh
+++ build-micronaut-consul-http-client.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Building micronaut-basic-app..."
+echo "Building micronaut-service-discovery-consul-http-client..."
 
 export JAVA_HOME=$CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home
 export PATH=$JAVA_HOME/bin:$PATH
 java -version
 native-image --version
 
-git clone https://github.com/micronaut-graal-tests/micronaut-basic-app
-cd micronaut-basic-app
+git clone https://github.com/micronaut-graal-tests/micronaut-service-discovery-consul-http-client
+cd micronaut-service-discovery-consul-http-client
 export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
 ./build-native-image.sh
