4d997a74e211b50bbdc422f0353cee47d4a41039
Iván López <lopez.ivan@gmail.com>
2019-01-18 17:32:56 +0100

    Add Micronaut-Zipking test application

---
+++ build-micronaut-zipkin-graal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Building micronaut-zipkin-graal..."
+
+export JAVA_HOME=$CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home
+export PATH=$JAVA_HOME/bin:$PATH
+java -version
+native-image --version
+
+git clone https://github.com/micronaut-graal-tests/micronaut-zipkin-graal
+cd micronaut-zipkin-graal
+export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
+./build-native-image.sh
