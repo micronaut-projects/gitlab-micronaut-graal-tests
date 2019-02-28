9366b35b36548e4c2752caa182049e0bb55e3917
Iván López <lopez.ivan@gmail.com>
2019-02-28 17:32:47 +0100

    Add CI jobs for micronaut-rabbitmq-graal

---
+++ build-micronaut-rabbitmq-graal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Building micronaut-rabbitmq-graal..."
+
+export JAVA_HOME=$CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home
+export PATH=$JAVA_HOME/bin:$PATH
+java -version
+native-image --version
+
+git clone https://github.com/micronaut-graal-tests/micronaut-rabbitmq-graal
+cd micronaut-rabbitmq-graal
+echo "Git commit: `git rev-parse HEAD`"
+export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
+./build-native-image.sh
