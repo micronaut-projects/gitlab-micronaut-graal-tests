92db3f67837ad88f443f0cae322812c9605b4cf8
Iván López <lopez.ivan@gmail.com>
2019-03-06 11:10:46 +0100

    Add Management & Metrics application

---
+++ build-micronaut-management-graal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Building micronaut-management-graal..."
+
+export JAVA_HOME=$CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home
+export PATH=$JAVA_HOME/bin:$PATH
+java -version
+native-image --version
+
+git clone https://github.com/micronaut-graal-tests/micronaut-management-graal
+cd micronaut-management-graal
+git checkout gitlab
+echo "Git commit: `git rev-parse HEAD`"
+export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
+./build-native-image.sh
