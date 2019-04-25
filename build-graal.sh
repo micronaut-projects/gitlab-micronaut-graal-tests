f318515101f4ecb37d841a387045be57afd2e7fa
Iván López <lopez.ivan@gmail.com>
2019-04-25 18:18:34 +0200

    Revert "Use openjdk1.8.0_202-jvmci-0.59"
    This reverts commit 257c5c524a4e4204e88ef8aa4ab7e2274cea7858.

--- build-graal.sh
+++ build-graal.sh
@@ -5,10 +5,10 @@ echo "Building Graal..."
 mkdir graal
 cd graal
 
-wget -q https://github.com/graalvm/openjdk8-jvmci-builder/releases/download/jvmci-0.59/openjdk-8u202-jvmci-0.59-linux-amd64.tar.gz
-tar zxf openjdk-8u202-jvmci-0.59-linux-amd64.tar.gz
+wget -q https://github.com/graalvm/openjdk8-jvmci-builder/releases/download/jvmci-0.58/openjdk-8u202-jvmci-0.58-linux-amd64.tar.gz
+tar zxf openjdk-8u202-jvmci-0.58-linux-amd64.tar.gz
 export PATH=$PWD/mx:$PATH
-export JAVA_HOME=$CI_PROJECT_DIR/graal/openjdk1.8.0_202-jvmci-0.59
+export JAVA_HOME=$CI_PROJECT_DIR/graal/openjdk1.8.0_202-jvmci-0.58
 
 git clone https://github.com/oracle/graal
 git clone https://github.com/graalvm/mx
