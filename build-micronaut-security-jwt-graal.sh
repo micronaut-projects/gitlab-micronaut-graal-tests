6f6590dc21c48fdc00781f58f30c9fa91c5edfac
Iván López <lopez.ivan@gmail.com>
2019-04-25 17:26:22 +0200

    Add Security-JWT application

---
+++ build-micronaut-security-jwt-graal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Building micronaut-security-jwt-graal..."
+
+export JAVA_HOME=$CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home
+export PATH=$JAVA_HOME/bin:$PATH
+java -version
+native-image --version
+
+git clone https://github.com/micronaut-graal-tests/micronaut-security-jwt-graal
+cd micronaut-security-jwt-graal
+echo "Git commit: `git rev-parse HEAD`"
+export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
+./build-native-image.sh
