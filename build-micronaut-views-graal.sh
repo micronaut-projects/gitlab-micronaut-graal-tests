eba54188b907361617a8be1efa3dfa3817b4a983
Iván López <lopez.ivan@gmail.com>
2019-06-06 15:25:47 +0200

    Split views repository in 4 jobs per stage

--- build-micronaut-views-graal.sh
+++ build-micronaut-views-graal.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "Building micronaut-views-graal..."
+TEMPLATE_TECHNOLOGY=$1
+
+if [ -z "$TEMPLATE_TECHNOLOGY" ]; then
+    echo "Need to provide the template technology: freemarker, handlebars, thymeleaf, velocity"
+    exit 1
+fi
+
+echo "Building micronaut-views-graal: ${TEMPLATE_TECHNOLOGY}..."
 
 export JAVA_HOME=$CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home
 export PATH=$JAVA_HOME/bin:$PATH
@@ -9,6 +16,7 @@ native-image --version
 
 git clone https://github.com/micronaut-graal-tests/micronaut-views-graal
 cd micronaut-views-graal
+git checkout $TEMPLATE_TECHNOLOGY
 echo "Git commit: `git rev-parse HEAD`"
 export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
 ./build-native-image.sh
