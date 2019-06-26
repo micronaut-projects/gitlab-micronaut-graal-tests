9f98c5afccb27e89b02fa4af58bad6f60fd262b5
Iván López <lopez.ivan@gmail.com>
2019-06-26 13:38:18 +0200

    Update to openjdk1.8.0_212-jvmci-20-b04

--- build-graal.sh
+++ build-graal.sh
@@ -5,10 +5,10 @@ echo "Building Graal..."
 mkdir graal
 cd graal
 
-wget -q https://github.com/graalvm/openjdk8-jvmci-builder/releases/download/jvmci-19-b01/openjdk-8u212-jvmci-19-b01-linux-amd64.tar.gz
-tar zxf openjdk-8u212-jvmci-19-b01-linux-amd64.tar.gz
+wget -q https://github.com/graalvm/openjdk8-jvmci-builder/releases/download/jvmci-20-b04/openjdk-8u212-jvmci-20-b04-linux-amd64.tar.gz
+tar zxf openjdk-8u212-jvmci-20-b04-linux-amd64.tar.gz
 export PATH=$PWD/mx:$PATH
-export JAVA_HOME=$CI_PROJECT_DIR/graal/openjdk1.8.0_212-jvmci-19-b01
+export JAVA_HOME=$CI_PROJECT_DIR/graal/openjdk1.8.0_212-jvmci-20-b04
 
 git clone https://github.com/oracle/graal
 git clone https://github.com/graalvm/mx
