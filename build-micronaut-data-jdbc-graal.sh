15c14b7aee16ff216bd0ae189672ea9f931abef8
Iván López <lopezi@objectcomputing.com>
2019-10-15 09:15:47 +0200

    Add Micronaut Data JDBC for Postgres and H2

--- build-micronaut-data-jdbc-graal.sh
+++ build-micronaut-data-jdbc-graal.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "Building micronaut-data-jdbc-graal..."
+DB_DIALECT=$1
+
+if [ -z "$DB_DIALECT" ]; then
+    echo "Need to provide the database dialect: h2, postgres"
+    exit 1
+fi
+
+echo "Building micronaut-data-jdbc-graal: ${DB_DIALECT}..."
 
 export JAVA_HOME=$CI_PROJECT_DIR/graal/graal/vm/latest_graalvm_home
 export PATH=$JAVA_HOME/bin:$PATH
@@ -9,6 +16,7 @@ native-image --version
 
 git clone https://github.com/micronaut-graal-tests/micronaut-data-jdbc-graal
 cd micronaut-data-jdbc-graal
+git checkout $DB_DIALECT
 echo "Git commit: `git rev-parse HEAD`"
 export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
 ./build-native-image.sh
--- test-micronaut-data-jdbc-graal-postgres.sh
+++ test-micronaut-data-jdbc-graal-postgres.sh
@@ -3,7 +3,7 @@ set -x
 
 apk add curl jq
 
-$CI_PROJECT_DIR/micronaut-data-jdbc-graal/mn-data-jdbc-graal &
+MICRONAUT_CONFIG_FILES=$CI_PROJECT_DIR/application-micronaut-data-jdbc-postgres.yml $CI_PROJECT_DIR/micronaut-data-jdbc-graal/mn-data-jdbc-graal-postgres &
 sleep 3
 
 RESPONSE=$(curl -s localhost:8080/owners)
