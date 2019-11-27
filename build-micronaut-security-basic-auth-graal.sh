22e4e4ae415a745a9e83ed114eb2aa1efde9df79
Iván López <lopezi@objectcomputing.com>
2019-11-27 15:27:47 +0100

    Add Security Basic Auth application

---
+++ build-micronaut-security-basic-auth-graal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Building micronaut-security-basic-auth-graal..."
+
+export JAVA_HOME=$CI_PROJECT_DIR/graal_dist
+export PATH=$JAVA_HOME/bin:$PATH
+java -version
+native-image --version
+
+git clone https://github.com/micronaut-graal-tests/micronaut-security-basic-auth-graal
+cd micronaut-security-basic-auth-graal
+echo "Git commit: `git rev-parse HEAD`"
+export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
+./build-native-image.sh
---
+++ test-security-basic-auth-graal.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -x
+
+apk add curl jq
+
+ls -l
+pwd
+ls -l $CI_PROJECT_DIR
+ls -l $CI_PROJECT_DIR/micronaut-security-basic-auth-graal
+
+
+$CI_PROJECT_DIR/micronaut-security-basic-auth-graal/security-basic-auth-graal &
+sleep 3
+
+RESPONSE=$(curl -s localhost:8080 --write-out %{http_code})
+EXPECTED_RESPONSE='401'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s localhost:8080 --user sherlock:wrongpassword --write-out %{http_code})
+EXPECTED_RESPONSE='401'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s localhost:8080 --user sherlock:password)
+EXPECTED_RESPONSE='sherlock'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
