f05277dd2998a3a21099efa98bce53305d9956e6
Iván López <lopezi@objectcomputing.com>
2019-12-03 11:38:41 +0100

    Add http status and http response tests for basic app

--- test-basic-app.sh
+++ test-basic-app.sh
@@ -9,3 +9,19 @@ sleep 3
 RESPONSE=$(curl -s localhost:8080/hello/Micronaut)
 EXPECTED_RESPONSE='{"msg":"Hello Micronaut"}'
 if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s localhost:8080/status/annotation --write-out %{http_code})
+EXPECTED_RESPONSE='400'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s localhost:8080/status/http-status --write-out %{http_code})
+EXPECTED_RESPONSE='400'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s localhost:8080/status/http-response-body --write-out %{http_code})
+EXPECTED_RESPONSE='{"message":"Error message","code":400}400'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s localhost:8080/status/mutable-http-response-body --write-out %{http_code})
+EXPECTED_RESPONSE='{"message":"Error message","code":400}400'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
