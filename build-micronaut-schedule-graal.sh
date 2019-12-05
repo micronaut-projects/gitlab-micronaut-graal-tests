f785af03d9e4cd8065b86bf877b6904a5333627d
Iván López <lopezi@objectcomputing.com>
2019-12-05 09:21:23 +0100

    Add `@Schedule` test application

---
+++ build-micronaut-schedule-graal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Building micronaut-schedule-graal..."
+
+export JAVA_HOME=$CI_PROJECT_DIR/graal_dist
+export PATH=$JAVA_HOME/bin:$PATH
+java -version
+native-image --version
+
+git clone https://github.com/micronaut-graal-tests/micronaut-schedule-graal
+cd micronaut-schedule-graal
+echo "Git commit: `git rev-parse HEAD`"
+export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
+./build-native-image.sh
---
+++ test-schedule.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x
+
+apk add curl
+
+$CI_PROJECT_DIR/micronaut-schedule-graal/mn-schedule-graal &
+sleep 5
+
+RESPONSE=$(curl -s localhost:8080/count)
+EXPECTED_RESPONSE='3'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
