8853754e73d11ca9afcea9d6e6508741a28ba43b
Iván López <lopezi@objectcomputing.com>
2020-03-06 11:46:41 +0100

    Add Micronaut Security Cookie application

---
+++ build-micronaut-security-cookie-graal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Building micronaut-security-cookie-graal..."
+
+export JAVA_HOME=$CI_PROJECT_DIR/graal_dist
+export PATH=$JAVA_HOME/bin:$PATH
+java -version
+native-image --version
+
+git clone https://github.com/micronaut-graal-tests/micronaut-security-cookie-graal
+cd micronaut-security-cookie-graal
+echo "Git commit: `git rev-parse HEAD`"
+export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
+./build-native-image.sh
