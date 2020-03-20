41a0196de564f0d247f4ec3e700f93e080c0f4e8
Iván López <lopezi@objectcomputing.com>
2020-03-20 16:41:15 +0100

    Add Security Session test application

---
+++ test-security-session-graal.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -x
+
+apk add curl jq
+
+$CI_PROJECT_DIR/micronaut-security-session-graal/security-session-graal &
+sleep 3
+
+# Unauthenticated request
+RESPONSE=$(curl 'http://localhost:8080/login' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Cookie: SESSION=ABCDEF123456' --data 'username=&password=' | jq -r '.message')
+EXPECTED_RESPONSE='Bad Request'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+# Authenticated request
+RESPONSE=$(curl 'http://localhost:8080/login' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Cookie: SESSION=ABCDEF123456' --data 'username=sherlock&password=password' -c session.txt --write-out %{http_code})
+EXPECTED_RESPONSE='303'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+if [ ! -f session.txt ]; then echo "session.txt should exist" && exit 1; fi
