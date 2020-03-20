7508211d0b750b53ded15436171108e037e57d3a
Iván López <lopezi@objectcomputing.com>
2020-03-20 16:41:15 +0100

    Add Security Session test application

---
+++ build-micronaut-security-session-graal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Building micronaut-security-session-graal..."
+
+export JAVA_HOME=$CI_PROJECT_DIR/graal_dist
+export PATH=$JAVA_HOME/bin:$PATH
+java -version
+native-image --version
+
+git clone https://github.com/micronaut-graal-tests/micronaut-security-session-graal
+cd micronaut-security-session-graal
+git checkout $CI_BUILD_REF_NAME
+echo "Git commit: `git rev-parse HEAD`"
+export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
+./build-native-image.sh
