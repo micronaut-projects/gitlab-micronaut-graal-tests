51974a536e0d111e07180cd11729d988eb3b2893
Iván López <lopezi@objectcomputing.com>
2020-03-24 17:29:34 +0100

    Add Introspected test application

---
+++ test-introspected.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x
+
+apk add curl
+
+$CI_PROJECT_DIR/micronaut-introspected-graal/mn-introspected-graal &
+sleep 3
+
+RESPONSE=$(curl -s localhost:8080/books)
+EXPECTED_RESPONSE='{"name":"Micronaut"}'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
