9ca10e80cf18f8530c3ad67ff88a4fd7cacc5b25
Iván López <lopezi@objectcomputing.com>
2020-04-27 09:11:06 +0200

    Upgrade to labsjdk-ce-11.0.7-jvmci-20.1-b02

--- build-graal.sh
+++ build-graal.sh
@@ -11,9 +11,9 @@ jdk8() {
 
 jdk11() {
     echo "Building GraalVM for JDK 11"
-    wget -q https://github.com/graalvm/labs-openjdk-11/releases/download/jvmci-20.0-b02/labsjdk-ce-11.0.6+9-jvmci-20.0-b02-linux-amd64.tar.gz
-    tar zxf labsjdk-ce-11.0.6+9-jvmci-20.0-b02-linux-amd64.tar.gz
-    export JAVA_HOME=${CI_PROJECT_DIR}/labsjdk-ce-11.0.6-jvmci-20.0-b02
+    wget -q https://github.com/graalvm/labs-openjdk-11/releases/download/jvmci-20.1-b02/labsjdk-ce-11.0.7+10-jvmci-20.1-b02-linux-amd64.tar.gz
+    tar zxf labsjdk-ce-11.0.7+10-jvmci-20.1-b02-linux-amd64.tar.gz
+    export JAVA_HOME=${CI_PROJECT_DIR}/labsjdk-ce-11.0.7-jvmci-20.1-b02
 }
 
 if [ "${JDK_VERSION}" == "jdk8" ]; then
