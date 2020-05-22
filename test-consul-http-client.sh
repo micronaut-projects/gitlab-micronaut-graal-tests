c69d546c9496f948637ddefc951cf39f3b7037ab
Iván López <lopezi@objectcomputing.com>
2020-05-22 13:18:30 +0200

    Use Consul 1.2.4 and enable integration tests

--- test-consul-http-client.sh
+++ test-consul-http-client.sh
@@ -6,11 +6,10 @@ apk add curl libstdc++
 MICRONAUT_CONFIG_FILES=$CI_PROJECT_DIR/application-consul-http-client.yml $CI_PROJECT_DIR/micronaut-service-discovery-consul-http-client/service-discovery &
 sleep 3
 
-# Consul fails con GitlabCI
-#RESPONSE=$(curl -s localhost:8080/hello/Micronaut)
-#EXPECTED_RESPONSE='Hello Micronaut'
-#if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
-#
-#RESPONSE=$(curl -s localhost:8080/api/hello/Micronaut)
-#EXPECTED_RESPONSE='Hello Micronaut'
-#if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+RESPONSE=$(curl -s localhost:8080/hello/Micronaut)
+EXPECTED_RESPONSE='Hello Micronaut'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s localhost:8080/api/hello/Micronaut)
+EXPECTED_RESPONSE='Hello Micronaut'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
