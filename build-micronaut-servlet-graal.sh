87b30215be132f93e80091e527c9d48d8befca37
Iván López <lopezi@objectcomputing.com>
2020-06-09 17:38:55 +0200

    Log full info for last commit and increase output for java version

--- build-micronaut-servlet-graal.sh
+++ build-micronaut-servlet-graal.sh
@@ -11,12 +11,17 @@ echo "Building micronaut-servlet-graal: ${SERVLET_ENGINE}..."
 
 export JAVA_HOME=$CI_PROJECT_DIR/graal_dist
 export PATH=$JAVA_HOME/bin:$PATH
+echo "------------------------------------"
 java -version
+echo "------------------------------------"
 native-image --version
+echo "------------------------------------"
 
 git clone https://github.com/micronaut-graal-tests/micronaut-servlet-graal
 cd micronaut-servlet-graal
 git checkout ${CI_BUILD_REF_NAME}_${SERVLET_ENGINE}
-echo "Git commit: `git rev-parse HEAD`"
+echo "------------------------------------"
+git log -1
+echo "------------------------------------"
 export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
 ./build-native-image.sh
