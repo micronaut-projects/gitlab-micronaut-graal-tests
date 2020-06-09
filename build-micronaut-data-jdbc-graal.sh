8f8ad89419e84794b046a0def56bf040251281de
Iván López <lopezi@objectcomputing.com>
2020-06-09 17:38:55 +0200

    Log full info for last commit and increase output for java version

--- build-micronaut-data-jdbc-graal.sh
+++ build-micronaut-data-jdbc-graal.sh
@@ -11,12 +11,17 @@ echo "Building micronaut-data-jdbc-graal: ${DB_DIALECT}..."
 
 export JAVA_HOME=$CI_PROJECT_DIR/graal_dist
 export PATH=$JAVA_HOME/bin:$PATH
+echo "------------------------------------"
 java -version
+echo "------------------------------------"
 native-image --version
+echo "------------------------------------"
 
 git clone https://github.com/micronaut-graal-tests/micronaut-data-jdbc-graal
 cd micronaut-data-jdbc-graal
 git checkout ${CI_BUILD_REF_NAME}_${DB_DIALECT}
-echo "Git commit: `git rev-parse HEAD`"
+echo "------------------------------------"
+git log -1
+echo "------------------------------------"
 export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
 ./build-native-image.sh
--- build-micronaut-service-discovery-consul.sh
+++ build-micronaut-service-discovery-consul.sh
@@ -4,12 +4,17 @@ echo "Building micronaut-service-discovery-consul..."
 
 export JAVA_HOME=$CI_PROJECT_DIR/graal_dist
 export PATH=$JAVA_HOME/bin:$PATH
+echo "------------------------------------"
 java -version
+echo "------------------------------------"
 native-image --version
+echo "------------------------------------"
 
 git clone https://github.com/micronaut-graal-tests/micronaut-service-discovery-consul
 cd micronaut-service-discovery-consul
 git checkout $CI_BUILD_REF_NAME
-echo "Git commit: `git rev-parse HEAD`"
+echo "------------------------------------"
+git log -1
+echo "------------------------------------"
 export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
 ./build-native-image.sh
