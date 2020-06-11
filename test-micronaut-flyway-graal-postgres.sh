4bf5eaed7a4b225f3e9ca798c6b9adc9ed645aa7
Iván López <lopezi@objectcomputing.com>
2020-06-11 11:48:16 +0200

    Add Flyway test app for H2, Postgres and MariaDB

---
+++ test-micronaut-flyway-graal-postgres.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x
+
+apk add curl jq libstdc++
+
+MICRONAUT_CONFIG_FILES=$CI_PROJECT_DIR/application-micronaut-flyway-mariadb.yml $CI_PROJECT_DIR/micronaut-flyway-graal/mn-flyway-graal-mariadb &
+sleep 3
+
+RESPONSE=$(curl -s localhost:8080/users)
+EXPECTED_RESPONSE='[{"id":1,"username":"ilopmar","firstName":"Iván","lastName":"López"},{"id":2,"username":"graemerocher","firstName":"Graeme","lastName":"Rocher"}]'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
