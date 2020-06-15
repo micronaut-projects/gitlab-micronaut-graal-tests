29a0b04e3be9deac064f0a83dd5050374b96670b
Iván López <lopezi@objectcomputing.com>
2020-06-15 11:17:03 +0200

    Add Micronaut Data JPA MySQL application

--- build-micronaut-data-jpa-graal.sh
+++ build-micronaut-data-jpa-graal.sh
@@ -3,7 +3,7 @@
 DB_DIALECT=$1
 
 if [ -z "$DB_DIALECT" ]; then
-    echo "Need to provide the database dialect: h2, postgres, mariadb"
+    echo "Need to provide the database dialect: h2, postgres, mariadb, mysql"
     exit 1
 fi
 
