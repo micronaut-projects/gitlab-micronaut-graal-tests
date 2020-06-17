15c768a32ccbfaa3b128fa44ede396b2d959319c
Iván López <lopezi@objectcomputing.com>
2020-06-17 12:12:38 +0200

    Add Elasticsearch app

---
+++ test-elasticsearch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -x
+
+apk add curl jq libstdc++
+
+MICRONAUT_CONFIG_FILES=$CI_PROJECT_DIR/application-elasticsearch.yml $CI_PROJECT_DIR/micronaut-elasticsearch-graal/micronaut-elasticsearch-graal &
+sleep 3
+
+curl -s -X POST -H 'Content-Type:application/json' -d '{"imdb":"matrix", "title":"The Matrix"}' localhost:8080/api/movies
+sleep 5 # wait so ES has time to save and index the document
+
+RESPONSE=$(curl -s "localhost:8080/api/movies?title=matrix" | jq -c '.hits[] | {index: .index, source: .source}')
+EXPECTED_RESPONSE='{"index":"micronaut.movies","source":"{\"imdb\":\"matrix\",\"title\":\"The Matrix\"}"}'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
