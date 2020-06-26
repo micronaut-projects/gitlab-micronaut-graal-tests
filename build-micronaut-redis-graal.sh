3decd7251b3635c8241874f3da47d8211b7a504d
Iván López <lopezi@objectcomputing.com>
2020-06-26 09:40:20 +0200

    Add Redis application

---
+++ build-micronaut-redis-graal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Building micronaut-redis-graal..."
+
+export JAVA_HOME=$CI_PROJECT_DIR/graal_dist
+export PATH=$JAVA_HOME/bin:$PATH
+echo "------------------------------------"
+java -version
+echo "------------------------------------"
+native-image --version
+echo "------------------------------------"
+
+git clone https://github.com/micronaut-graal-tests/micronaut-redis-graal
+cd micronaut-redis-graal
+git checkout $CI_BUILD_REF_NAME
+echo "------------------------------------"
+git log -1
+echo "------------------------------------"
+export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
+./build-native-image.sh
