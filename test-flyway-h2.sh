238ca9bac84d184bcc6e42e367617ff87665d039
Iván López <lopezi@objectcomputing.com>
2020-10-20 12:56:57 +0200

    Test /flyway management endpoint

--- test-flyway-h2.sh
+++ test-flyway-h2.sh
@@ -9,3 +9,7 @@ sleep 3
 RESPONSE=$(curl -s localhost:8080/users)
 EXPECTED_RESPONSE='[{"id":1,"username":"ilopmar","firstName":"Iván","lastName":"López"},{"id":2,"username":"graemerocher","firstName":"Graeme","lastName":"Rocher"}]'
 if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s localhost:8080/flyway | jq -c '.[0] | [.migrations[] | {state, script} ]')
+EXPECTED_RESPONSE='[{"state":"SUCCESS","script":"databasemigrations/V1__init.sql"},{"state":"SUCCESS","script":"databasemigrations/V2__testdata.sql"},{"state":"SUCCESS","script":"other/V3__testdata2.sql"}]'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
