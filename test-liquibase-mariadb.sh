a805b2b25fa5e63ce730c7311fa3ff93312e3f6e
Iván López <lopezi@objectcomputing.com>
2020-11-27 10:03:00 +0100

    Add Liquibase test application

---
+++ test-liquibase-mariadb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -x
+
+apk add curl jq libstdc++
+
+export DATASOURCES_DEFAULT_URL=jdbc:mariadb://mariadbhost:3306/users
+$CI_PROJECT_DIR/micronaut-liquibase-graal/liquibase-mariadb &
+sleep 3
+
+RESPONSE=$(curl -s localhost:8080/users)
+EXPECTED_RESPONSE='[{"id":1,"username":"ilopmar","firstName":"Iván","lastName":"López"},{"id":2,"username":"graemerocher","firstName":"Graeme","lastName":"Rocher"}]'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s localhost:8080/liquibase | jq  -c '.[0] | [.changeSets[] | {execType, changeLog} ]')
+EXPECTED_RESPONSE='[{"execType":"EXECUTED","changeLog":"db/changelog/01-create-users-table.xml"},{"execType":"EXECUTED","changeLog":"db/changelog/02-insert-users-data.xml"}]'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
