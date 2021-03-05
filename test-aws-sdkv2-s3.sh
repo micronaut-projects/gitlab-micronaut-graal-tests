452a41bb40052a559800fd82de856c93be849e8b
Iván López <lopezi@objectcomputing.com>
2021-03-05 12:36:10 +0100

    Add AWS SDK v2 S3 test application

---
+++ test-aws-sdkv2-s3.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -x
+
+$CI_PROJECT_DIR/micronaut-aws-sdk2-graal/aws-s3 &
+sleep 3
+
+BUCKET_NAME="tmp-micronaut"-${CI_BUILD_ID}
+
+RESPONSE=$(curl -s -X POST localhost:8080/s3/buckets/$BUCKET_NAME | jq -r '.status')
+EXPECTED_RESPONSE='200'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s -X GET localhost:8080/s3/buckets | jq '.buckets[]' | grep $BUCKET_NAME)
+EXPECTED_RESPONSE="\"${BUCKET_NAME}\""
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s -X DELETE localhost:8080/s3/buckets/$BUCKET_NAME | jq -r '.status')
+EXPECTED_RESPONSE='204'
+if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
