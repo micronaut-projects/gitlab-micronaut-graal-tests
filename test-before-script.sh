b8bb4d46fc6ad7f0a1a93fb67216c8268c3da32f
Iván López <lopezi@objectcomputing.com>
2021-03-22 15:42:39 +0100

    Only use distroless image for basic-app

--- test-before-script.sh
+++ test-before-script.sh
@@ -1,5 +1,3 @@
-#!/busybox/sh
+#!/bin/sh
 
-wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O /usr/bin/jq && chmod +x /usr/bin/jq
-wget https://github.com/moparisthebest/static-curl/releases/download/v7.74.0/curl-amd64 -O /usr/bin/curl && chmod +x /usr/bin/curl
-ln -s /busybox/sh /bin/sh
+apk add curl jq libstdc++
