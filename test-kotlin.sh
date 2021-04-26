83b4c64f5b00eafe088803058760286458c1d119
Iván López <lopezi@objectcomputing.com>
2021-04-26 11:29:58 +0200

    Add tests for receiving and returning an enum

--- test-kotlin.sh
+++ test-kotlin.sh
@@ -7,3 +7,7 @@ sleep 3
 RESPONSE=$(curl -s localhost:8080/hello/Kotlin)
 EXPECTED_RESPONSE='{"msg":"Hello Kotlin"}'
 if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+
+RESPONSE=$(curl -s localhost:8080/color/blue)
+EXPECTED_RESPONSE_CONTAINS='BLUE'
+if [ "$RESPONSE" == "${RESPONSE%"$EXPECTED_RESPONSE_CONTAINS"*}" ]; then echo $RESPONSE && exit 1; fi
