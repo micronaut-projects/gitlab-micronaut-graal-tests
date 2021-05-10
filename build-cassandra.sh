b3a3f23c788086fa61395399cbd47842e11c41f5
Iván López <lopezi@objectcomputing.com>
2021-05-10 15:35:36 +0200

    Add Cassandra app

---
+++ build-cassandra.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Building micronaut-cassandra-graal..."
+
+export JAVA_HOME=$CI_PROJECT_DIR/graal_dist
+export PATH=$JAVA_HOME/bin:$PATH
+echo "------------------------------------"
+java -version
+echo "------------------------------------"
+native-image --version
+echo "------------------------------------"
+
+git clone https://github.com/micronaut-graal-tests/micronaut-cassandra-graal
+cd micronaut-cassandra-graal
+git checkout $APP_BRANCH
+echo "------------------------------------"
+git log -1
+echo "------------------------------------"
+export GRADLE_OPTS="-Dorg.gradle.daemon=false -Dorg.gradle.console=plain"
+./build-native-image.sh
