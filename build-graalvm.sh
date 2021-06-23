597d9eba500773ddb3b777f461c9a1254f94e686
Iván López <lopezi@objectcomputing.com>
2021-06-23 12:36:29 +0200

    Fix build GraalVM

--- build-graalvm.sh
+++ build-graalvm.sh
@@ -6,6 +6,7 @@ GRAALVM_BRANCH="master"
 downloadJdk() {
     JAVA_VERSION=$1
     cd ${GRAALVM_DIR}/graaljs
+    export MX_PYTHON_VERSION=3
     export JAVA_HOME=`mx -y fetch-jdk --to ${CI_PROJECT_DIR} --configuration common.json --java-distribution ${JAVA_VERSION} | tail -1 | sed 's/export JAVA_HOME=//'`
 }
 
