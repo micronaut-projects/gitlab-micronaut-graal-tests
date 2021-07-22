366d51871c942eed4b620096df5b772c747ae4ea
Iván López <lopezi@objectcomputing.com>
2021-07-22 15:16:39 +0200

    Fix JDBC tests (this time for real)
    Make curl outputs always in the same order processing the output with jq
    so if the order change because of the implementation the tests won't
    fail

--- test-data-jdbc-h2.sh
+++ test-data-jdbc-h2.sh
@@ -4,18 +4,18 @@ set -x
 $CI_PROJECT_DIR/micronaut-data-jdbc-graal/data-jdbc-h2 &
 sleep 3
 
-RESPONSE=$(curl -s localhost:8080/owners)
-EXPECTED_RESPONSE='[{"age":45,"name":"Fred","id":1},{"age":40,"name":"Barney","id":2}]'
+RESPONSE=$(curl -s localhost:8080/owners | jq -c '[.[] | {id, name, age}]')
+EXPECTED_RESPONSE='[{"id":1,"name":"Fred","age":45},{"id":2,"name":"Barney","age":40}]'
 if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
 
-RESPONSE=$(curl -s localhost:8080/owners/Fred)
+RESPONSE=$(curl -s localhost:8080/owners/Fred | jq -c '{id, name, age}')
 EXPECTED_RESPONSE='{"id":1,"name":"Fred","age":45}'
 if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
 
-RESPONSE=$(curl -s 'localhost:8080/pets?sort=name')
+RESPONSE=$(curl -s 'localhost:8080/pets?sort=name' | jq -c '[.[] | {name}]')
 EXPECTED_RESPONSE='[{"name":"Baby Puss"},{"name":"Dino"},{"name":"Hoppy"}]'
 if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
 
-RESPONSE=$(curl -s localhost:8080/pets/Dino | jq -c '{name, owner, type}')
+RESPONSE=$(curl -s localhost:8080/pets/Dino | jq -c '{name, owner: {id: .owner.id, name: .owner.name, age: .owner.age}, type}')
 EXPECTED_RESPONSE='{"name":"Dino","owner":{"id":1,"name":"Fred","age":45},"type":"DOG"}'
 if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
