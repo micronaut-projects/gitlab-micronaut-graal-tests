1562bb013b92f1b2e79874c6a9987b6ef3bdd63d
Iván López <lopezi@objectcomputing.com>
2021-09-23 10:53:58 +0200

    Use GraalVM 21.2.0

--- build-graalvm.sh
+++ build-graalvm.sh
@@ -1,54 +1,32 @@
 #!/bin/bash
 
 JDK_VERSION=$1
-GRAALVM_BRANCH="master"
+GRAALVM_VERSION="21.2.0"
 
-downloadJdk() {
+apt update && apt install jq -y
+
+downloadGraalVM() {
     JAVA_VERSION=$1
-    cd ${GRAALVM_DIR}/graal
-    export MX_PYTHON_VERSION=3
-    export JAVA_HOME=`mx -y fetch-jdk --to ${CI_PROJECT_DIR} --configuration ${GRAALVM_DIR}/graaljs/common.json --java-distribution ${JAVA_VERSION} | tail -1 | sed 's/export JAVA_HOME=//'`
+    echo "===================== Downloading GraalVM release ${GRAALVM_VERSION} for ${JAVA_VERSION}..."
+    GRAALVM_NAME="graalvm-ce-${JAVA_VERSION}-linux-amd64-"${GRAALVM_VERSION}".tar.gz"
+    wget -q `echo https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${GRAALVM_VERSION}/${GRAALVM_NAME}`
+    tar zxf `echo $GRAALVM_NAME`
+
+    JDK_DIRECTORY=`ls -1d graalvm* | head -1`
+
+    export JAVA_HOME=${CI_PROJECT_DIR}/${JDK_DIRECTORY}
+    $JAVA_HOME/bin/gu install native-image
 }
 
-mkdir graal
-cd graal
-export GRAALVM_DIR=`pwd`
-export PATH=$PWD/mx:$PATH
-
-git clone --branch ${GRAALVM_BRANCH} https://github.com/oracle/graal
-git clone --branch ${GRAALVM_BRANCH} https://github.com/graalvm/graaljs
-git clone --depth=1 https://github.com/graalvm/mx
-
 if [ "$JDK_VERSION" == "jdk11" ]; then
-    downloadJdk labsjdk-ce-11
-elif [ "$JDK_VERSION" == "jdk17" ]; then
-    downloadJdk labsjdk-ce-17
+    downloadGraalVM "java11"
+elif [ "$JDK_VERSION" == "jdk16" ]; then
+    downloadGraalVM "java16"
 else
-    echo "Need to provide a valid JDK version: jdk11, jdk17"
+    echo "Need to provide a valid JDK version: jdk11, jdk16"
     exit 1
 fi
 
-echo "------------------------------------"
-echo "GraalVM branch: ${GRAALVM_BRANCH}"
-${JAVA_HOME}/bin/java -version
-echo "------------------------------------"
-
-cd ${GRAALVM_DIR}/graaljs/graal-js
-echo "------------------------------------"
-echo "Building Graal-JS"
-git log -1
-echo "------------------------------------"
-mx clean
-mx --dynamicimports /compiler build
-
-cd ${GRAALVM_DIR}/graal/vm
-echo "------------------------------------"
-echo "Building GraalVM"
-git log -1
-echo "------------------------------------"
-mx clean
-mx --disable-polyglot --disable-libpolyglot --dynamicimports /substratevm,/graal-js --force-bash-launchers=native-image-configure,gu,polybench --skip-libraries=true build
-
-# Copy Graal SDK to new directory defined as artifact/cache
-echo "Copying Graal SDK to ${CI_PROJECT_DIR}/graal_dist..."
-cp -R ${GRAALVM_DIR}/graal/vm/latest_graalvm_home/ $CI_PROJECT_DIR/graal_dist
+# Copy GraalVM dist to new directory defined as artifact/cache
+echo "Copying GraalVM dist to ${CI_PROJECT_DIR}/graal_dist..."
+cp -R $JAVA_HOME/ $CI_PROJECT_DIR/graal_dist
